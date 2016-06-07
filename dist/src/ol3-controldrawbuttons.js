804f57db219fa76decde98321e5ea6143e660faa
Stephane Meaudre <smeaudre@kaliop.com>
2016-06-07 17:31:14 +0200

    TODO

--- dist/src/ol3-controldrawbuttons.js
+++ dist/src/ol3-controldrawbuttons.js
@@ -4,6 +4,8 @@ import dataLayers from './dataLayers';
 import ol from 'openlayers';
 import olMap from './openlayers';
 
+// TODO TRY : http://materialdesignblog.com/material-design-floating-action-button-for-web-that-really-stands-out/
+
 /**
  * OpenLayers 3 Draw Control, special fork for Kuzzle
  * Fork from github.com/HamHamFonFon/ol3-drawButtons
@@ -527,6 +529,13 @@ var ol3buttons = {
         buttonSquare.type_control = 'draw';
         buttonSquare.addEventListener('click', this.handleButtonsClick, false);
         elementDrawButtons.push(buttonSquare);
+        /*
+         <svg style="width:24px;height:24px;">
+         <path fill="#FFF" d="M 2,2L 8,2L 8,4L 16,4L 16,2.00001L 22,2.00001L 22,8L 20,8L 20,16L 22,16L 22,22L 16,22L 16,20L 8,20L 8,22L 2,22L 2,16L 4,16L 4,8L 2,8L 2,2 Z M 16,8L 16,6L 8,6L 8,8L 6,8L 6,16L 8,16L 8,18L 16,18L 16,16L 18,16L 18,8L 16,8 Z M 4,4.00001L 4,6.00001L 5.99999,6.00001L 5.99999,4.00001L 4,4.00001 Z M 18,4.00001L 18,6.00001L 20,6.00001L 20,4.00001L 18,4.00001 Z M 4,18L 4,20L 6,20L 6,18L 4,18 Z M 18,18L 18,20L 20,20L 20,18L 18,18 Z "></path>
+         </svg>
+         */
+
+
 
         // Circle
         var buttonCircle = this.buttonCircle = document.createElement('button');
